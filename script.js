e603ec9ad895e7a6f72d7b275706bc4dd9e25c21
ceza01 <123430152+ceza01@users.noreply.github.com>
2024-01-26 13:25:07 -0300

    added a function to create the 16x16 divs for the grid

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const div = document.createElement("div");
+/*const div = document.createElement("div");
 div.id = "grid";
-container.appendChild(div);
+container.appendChild(div);*/
+
+
+for (let i = 0; i <= 16; i++){
+    for (let j = 0; j <= 16; j++){
+        const newDiv = document.createElement('div');
+        newDiv.id = 'grid';
+        container.appendChild(newDiv);
+
+    }
+}
+
+
