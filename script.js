fb7acb4457240976d1fa41dfe07604bd428079f9
ceza01 <123430152+ceza01@users.noreply.github.com>
2024-01-29 21:42:42 -0300

    create a mouseover function to color each div when on hover

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 div.id = "grid";
 container.appendChild(div);*/
 
-
+// create 16x16 divs
 for (let i = 1; i <= 16; i++){
     for (let j = 1; j <= 16; j++){
         const newDiv = document.createElement('div');
         newDiv.id = 'grid';
-        newDiv.classList.add('color-test');
+        newDiv.classList.add('color');
         container.appendChild(newDiv);
 
     }
 }
 
 
+let colorHoover = ["pink", "purple", "red", "yellow"];
+const grids = document.querySelectorAll(".color");
 
-
+grids.forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+        grid.style.backgroundColor = colorHoover[(Math.floor(Math.random() * colorHoover.length))];
+    })
+    
+});
